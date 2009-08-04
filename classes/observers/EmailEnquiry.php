9ff6e83d89626b54c3910db227c3944a78d3ec03
Yevgeniy A. Viktorov <wik@osmonitoring.com>
2009-08-04 08:30:14 +0300

    add EmailEnquiry observer, non functional prototype

---
+++ classes/observers/EmailEnquiry.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Seagull PHP Framework
+ *
+ * LICENSE
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *
+ * o Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * o Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * o The names of the authors may not be used to endorse or promote
+ *   products derived from this software without specific prior written
+ *   permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * PHP version 5
+ *
+ * @category  Modules
+ * @package   Enquiry
+ * @author    Yevgeniy A. Viktorov <wik@osmonitoring.com>
+ * @copyright 2009 Demian Turner
+ * @license   http://opensource.org/licenses/bsd-license.php BSD License
+ * @version   SVN: $Id$
+ * @link      http://github.com/yviktorov/seagull_module_enquiry/tree/master
+ */
+
+/**
+ * Enquiry observer to email data to administrator
+ *
+ * @category Modules
+ * @package  Enquiry
+ * @author   Yevgeniy A. Viktorov <wik@osmonitoring.com>
+ * @license  http://opensource.org/licenses/bsd-license.php BSD License
+ * @link     http://github.com/yviktorov/seagull_module_enquiry/tree/master
+ */
+class EmailEnquiry extends SGL_Observer
+{
+    /**
+     * update
+     *
+     * @param SGL_Observable $observable Observable object
+     *
+     * @access public
+     * @return void
+     */
+    public function update($observable)
+    {
+        // email enquiry
+        // $observable->output->wizardData;
+        $bEmailSent = true;
+        if (!$bEmailSent) {
+            return SGL::raiseError('Problem sending email', SGL_ERROR_EMAILFAILURE);
+        }
+    }
+}
